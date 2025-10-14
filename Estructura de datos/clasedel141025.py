aee96ef7b15937cb728f8c0cfe630b29eccaf6e2
AbelSolanoS <abelsolanosantos@gmail.com>
2025-10-14 19:43:17 -0400

    up1

---
+++ Estructura de datos/clasedel141025.py
@@ -0,0 +1,6 @@
+numero1=int(input("Ingrese un numero: "))
+numero2=int(input("Ingrese otro numero: "))
+if numero1>numero2:
+    print(f"El numero mayor es: {numero1} es mayor que {numero2}")
+else:
+    print (f"El numero mayor es: {numero2} es mayor que {numero1}")
--- prueba.py
+++
@@ -1,4 +0,0 @@
-num1 = input("Inserta el primer número: ")
-num2 = input("Inserta el segundo número: ")
-re = int(num1) + int(num2)
-print("El resultado es: "  + str(re))
