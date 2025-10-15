9182bda1ad5e6031a6bf051a9f9d5ab722725b20
AbelSolanoS <abelsolanosantos@gmail.com>
2025-10-14 20:29:57 -0400

    guardados

---
+++ Estructura de datos/BucleFor.py
@@ -0,0 +1,4 @@
+x=1 #1 a 100
+
+for x in range(200,310,10):
+    print(x)    
---
+++ Estructura de datos/Buclefor2.py
@@ -0,0 +1,6 @@
+suma=0
+for x in range(10):
+    numero=int(input("Introduce un numero: "))
+    suma+=numero
+    promedio=suma/10
+    print(f"la suma es {suma} y el promedio es {promedio}")
